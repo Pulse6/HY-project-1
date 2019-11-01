10b0df1c9e0191f68a59629b4ecb9868fd30bf4b
Pulse6 <50462559+Pulse6@users.noreply.github.com>
2019-11-01 15:27:04 -0400

    Add files via upload
    Not responsive

---
+++ script.js
@@ -0,0 +1,6 @@
+// const meniIcone = document.querySelector ("hamburger-menu");
+// const navbar = document.querySelector(".navbar");
+
+// menuIcone.addEventListener("click" , () => {
+//     navbar.classList.toggle("change");
+// });
